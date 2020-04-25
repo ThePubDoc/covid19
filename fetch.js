eecc86bb09508f95abe5ec06157f17d3e1154253
Aayush Agrawal <aayush9152@gmail.com>
2020-04-25 13:38:23 +0530

    first commit

---
+++ fetch.js
@@ -0,0 +1,41 @@
+var table;
+const get = (async()=> {
+    var response = await fetch("https://covid19-api.com/country/all?format=json", {
+        "method": "GET"
+        }
+    )
+    
+    const ar = await response.json();
+    // ar.sort( compare );
+    // console.log(ar[0])
+    
+    table = new Tabulator("#example-table", {
+        data:ar,
+        layout:"fitColumns",
+        pagination:"local",
+        paginationSize:10,
+        responsiveLayout:"hide",
+        initialSort:[
+            {column:"confirmed", dir:"dsc"},
+        ],
+        columns:[
+            {title:"Country",field:"country"},
+            {title:"Confirmed", field:"confirmed"},
+            {title:"Recovered",field:"recovered"},
+            {title:"Deaths",field:"deaths"},
+            {title:"Critical",field:"critical"},            
+        ],
+    });
+    
+})
+
+const updateFilter = (() => {
+    let value = document.getElementById("filter").value;
+    console.log(value)
+    table.setFilter("country", "like" , value)
+})
+
+get();
+document.getElementById("filter").addEventListener("keyup", updateFilter);
+
+
